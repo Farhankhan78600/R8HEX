bc4b60d63e4f0d9dab25931159657935a82b5fde
Farhankhan78600 <121998339+Farhankhan78600@users.noreply.github.com>
2025-04-13 16:04:11 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "7880809396:AAG9ugPqJcGniUeVgQFne_mzbjyH6IEwR_o",
+  token: "7792086541:AAEp2UY608NcCsSSm4XRuLUglUupiA1k3Rw",
   id:"8150418484",
   address:"https://r8hex-1.onrender.com"
 };
